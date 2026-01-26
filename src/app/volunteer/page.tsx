0558d71eda4073c97246cc820de6b949ffd755dd
Eddy Luo <6147723+edluo1@users.noreply.github.com>
2026-01-25 16:43:13 -0800

    Updated volunteer signup (for now)
    Old volunteer form has technical issues

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -16,9 +16,31 @@ export default function Volunteer() {
                         <li>... & More!</li>
                     </ul>
                     <p className="mt-14 lg:mt-20">
-                        Click <span><a className="text-blue-500" href="https://docs.google.com/forms/d/e/1FAIpQLScKSmgHNqrJ50aL7OyEs5vO82k2IfZeQv8SD5wFFKXkJqX4Ew/viewform" target="_blank">here</a></span> to get started!
+                        Email us at <span><a href="seattlecommunityfridge@gmail.com" target="_blank">seattlecommunityfridge@gmail.com</a></span> to get started!
                     </p>
                 </div>
+            </div>
+            <div className="block w-10/12 mx-auto">
+                <h2 className="text-3xl sm:text-4xl mt-8 font-SauceTomato">Include these details:</h2>
+                <ul className="list-disc">
+                    <li>Name and Pronouns</li>
+                    <li>Preferred method of contact (text message, email, or Instagram) (include the number, address, or account for us to contact you!)</li>
+                    <li>Discord username (we use it to communicate!)</li>
+                    <li>Help you'd like to provide, including:
+                        <ul className="list-disc ml-5">
+                            <li>Food donation pick-ups + fridge cleaning</li>
+                            <li>Shelter building + fridge transport</li>
+                            <li>Host/food donor outreach</li>
+                            <li>Volunteer coordination</li>
+                            <li>Social media</li>
+                            <li>and more!</li>
+                        </ul>
+                    </li>
+                    <li>Optionally, let us know if you work with other groups that combat food insecurity or other Seattle organizations/mutual aid networks!</li>
+                </ul>
+
+                <p>Check our <a href="/faq">FAQ page</a> if you have any questions or want to know more about SCF.</p>
+                <p>If you applied earlier using a Google form and haven't gotten a response, please re-apply. We're experiencing technical difficulties with the old form.</p>
             </div>
         </section>
     )
