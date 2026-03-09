edded4563e8d2533840ffbb16d4765bdee51a0ae
edluo1 <6147723+edluo1@users.noreply.github.com>
2026-03-09 13:23:33 -0700

    Updated location of fridge, removed "upcoming events" section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,17 +46,6 @@ export default function Home() {
               </p>
           </div>
         </div>
-        <h2 className="text-2xl md:text-5xl mt-14 font-SauceTomato text-blue-500">UPCOMING EVENTS</h2>
-        <div className="w-full flex justify-center my-8">
-          <iframe 
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&showTz=0&showCalendars=0&showDate=0&showPrint=0&src=NDhiY2FlMDM0ZTc2MzRlNjM2YmYyZmQwMTJhNmY2MTZlMjQ5Y2YxMTlkYzg5Y2QzY2NiNzNkNDgwMzZmMTc4NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23ef6c00" 
-            style={{ border: 0 }} 
-            width="800" 
-            height="400" 
-            frameBorder="0" 
-            scrolling="no"
-          />
-        </div>
       </main>
     </div>
   );
